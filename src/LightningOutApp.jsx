cf13fac90f6e8d4ec654488d136d73a09f01b508
Jatin Hasija <122212679+jatinhasija02@users.noreply.github.com>
2026-02-03 11:50:15 +0530

    Update LightningOutApp.jsx

--- src/LightningOutApp.jsx
+++ src/LightningOutApp.jsx
@@ -1,41 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const LightningOutApp = () => {
+  const [dynamicUrl, setDynamicUrl] = useState("");
+
   useEffect(() => {
-    // Load Lightning Out script dynamically
+    // 1. Ask our "Secret Agent" file for a fresh URL
+    fetch("/api/get-url")
+      .then(res => res.json())
+      .then(data => setDynamicUrl(data.url));
+
+    // 2. Load the Salesforce Script
     const script = document.createElement("script");
-    script.src =
-      "https://algocirrus-b6-dev-ed.develop.my.salesforce.com/lightning/lightning.out.latest/index.iife.prod.js";
+    script.src = "https://algocirrus-b6-dev-ed.develop.my.salesforce.com/lightning/lightning.out.latest/index.iife.prod.js";
     script.async = true;
-
-    script.onload = () => {
-      const lightningOutApp = document.querySelector(
-        "lightning-out-application"
-      );
-
-      if (lightningOutApp) {
-        lightningOutApp.addEventListener("ready", () => {
-          console.log("Success! LWC is rendered via Lightning Out 2.0");
-        });
-      }
-    };
-
     document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
   }, []);
+
+  // Don't show anything until we have the URL
+  if (!dynamicUrl) return <div>Connecting to Salesforce...</div>;
 
   return (
     <>
       <lightning-out-application
         components="c-hello-world-lwc"
-        frontdoor-url="https://algocirrus-b6-dev-ed.develop.my.salesforce.com/secur/frontdoor.jsp?ssid=00DNS00000NKw5p!AQEAQIsqSxMbz8O0NATSse1cxl8LiFSIHapDPGRoTJZFk9Zlo_sj0bfbVIznNKuaB6y3bcYF69fvcMrKRsxnkKqqTz6J4Zoh"
+        frontdoor-url={dynamicUrl}
         app-id="1UsNS0000000CUD0A2"
       ></lightning-out-application>
 
-      {/* LWC Web Component */}
       <c-hello-world-lwc></c-hello-world-lwc>
     </>
   );
