984c56020b41e67b4cd41725c623b1e4bf018f6b
Jatin Hasija <hasijajatin02@gmail.com>
2026-02-04 17:48:36 +0530

    new branchv4

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,14 @@
-// src/App.jsx
-import { useEffect, useState } from 'react';
 import './App.css';
-import { account } from './lib/appwrite';
-import Login from './components/Login';
+import SalesforceLWC from './SalesforceLWC';
 import LightningOutApp from './LightningOutApp';
-
 function App() {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Check if the user is logged into Appwrite via Okta
-    account.get()
-      .then((res) => {
-        setUser(res);
-        setLoading(false);
-      })
-      .catch(() => {
-        setLoading(false);
-      });
-  }, []);
-
-  if (loading) return <div style={{ color: 'white', textAlign: 'center', marginTop: '100px' }}>Checking Session...</div>;
 
   return (
     <>
-      {!user ? (
-        <Login /> 
-      ) : (
-        /* PASSING THE EMAIL HERE IS CRITICAL */
-        <LightningOutApp userEmail={user.email} /> 
-      )}
+     {/* <SalesforceLWC/> */}
+     <LightningOutApp/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App 
